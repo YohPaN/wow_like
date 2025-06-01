085a7f3277d905d93765bc2765a81766a3457f5e
Booker <op.anthony.pro@gmail.com>
2025-06-01 15:34:38 +0200

    OUI

--- Assets/Scripts/EnnemyController.cs
+++ Assets/Scripts/EnnemyController.cs
@@ -7,6 +7,7 @@ namespace Assets.Scripts
     {
         [SerializeField] PlayerHealthUI healthUI;
         public int currentHealth = 100;
+        
         public int maxHealth = 100;
         public float chaseRange = 10f;
         public float attackRange = 30f;
